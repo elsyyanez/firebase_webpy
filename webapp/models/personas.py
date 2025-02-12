bc7b4c302ed0fe7d3e93734c7b2a4f64e327c6e4
elsyyanez <160617628+elsyyanez@users.noreply.github.com>
2025-02-12 00:14:16 +0000

    Conexion de firebases en personas.py

--- webapp/models/personas.py
+++ webapp/models/personas.py
@@ -11,5 +11,26 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
 class Personas:
+
     def __init__(self):
-      pass
+        pass
+
+    def lista_personas(self):
+        try:
+            personas = db.child("personas").get()
+            response = {
+                "status": 200,
+                "message": "Todo bien :3",
+                "personas": dict(personas.val())  # Se corrigió el error de sintaxis
+            }
+            return response
+        except Exception as error:
+            response = {
+                "status": 400,
+                "message": "Error en el servidor",
+                "personas": {}
+            }
+            return response  # Se corrigió la indentación
+
+persona = Personas()
+print(f"{persona.lista_personas()}")
